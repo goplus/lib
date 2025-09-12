aa5ba1ecf02d68b07cf3fe4578a023ee416c71d1
luoliwoshang <2643523683@qq.com>
2025-09-12 15:54:50 +0800

    update demo

--- _demo/esp/main.go
+++ _demo/esp/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"unsafe"
+	_ "unsafe"
+
+	"github.com/goplus/lib/emb/machine"
 )
 
 //go:linkname StoreUint32 llgo.atomicStore
 func StoreUint32(addr *uint32, val uint32)
 
 //go:linkname sleep sleep
-func sleep(tm int)
+func sleep(n int)
 
 func main() {
 	StoreUint32((*uint32)(unsafe.Pointer(uintptr(0x3ff480A4))), 0x50D83AA1)
@@ -16,9 +19,13 @@ func main() {
 	StoreUint32((*uint32)(unsafe.Pointer(uintptr(0x3ff5f048))), 0)
 	buttonPin := machine.GPIO34
 	buttonPin.Configure(machine.PinConfig{Mode: machine.PinInput})
-	println(buttonPin.Get())
-	sleep(2)
-	println(buttonPin.Get())
-	println(buttonState)
-	// time.Sleep(200 * time.Millisecond)
+
+	for i := 0; i < 10; i++ {
+		if buttonPin.Get() {
+			println("yes")
+		} else {
+			println("no")
+		}
+		sleep(1)
+	}
 }
